9e6791036c63f2c2e30a08be0b70393f07ef4bb8
nwdavis <nathanieldavis89@gmail.com>
2017-07-14 22:19:01 -0400

    issues with moment

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-
 var config = {
     apiKey: "AIzaSyALBCC9G099FgUtPopNgecipp3zXQaPMp0",
     authDomain: "clicking-things-e3110.firebaseapp.com",
@@ -12,30 +11,25 @@ var config = {
 
 firebase.initializeApp(config);
 
-var database = firebase.database();
+var db = firebase.database();
 
-var trains = [];
-
-//create click event on submit to gather values for entry
-//check that those are valid entries for the fields
-//add those values to an object
-//push that object to array trains
+//create click event on submit button to gather values for entry
+//add those values to an object newTrain
+//push newTrain to 
 
 $("#submit").on("click", function(event){
 	event.preventDefault();
-	var newTrain = [];
+	var newTrain = {};
 	newTrain.name = $("#trainName").val().trim();
 	newTrain.dest = $("#trainDest").val().trim();
 	newTrain.firstTrain = $("#firstTrain").val().trim();
 	newTrain.freq = $("#trainFreq").val().trim();
 	console.log(newTrain)
-	trains.push(newTrain)
+	
 
 	if (isNaN(newTrain.freq) === false) {
-		console.log("both params met")
-		database.ref().set({
-		allTrains: trains,
-	});
+		
+		db.ref().push(newTrain);
 
 	} else {
 		alert("Please check your entries and try again.")
@@ -46,17 +40,51 @@ $("#submit").on("click", function(event){
 
 
 //firebase watcher that updates HTML
+db.ref().on("child_added", function(snap){
 
+	//calcNextTrain will take the value of the first train, the current time, and the interval, and return the time of the next train
 
+	function calcNextTrain() {
+		//first train value
+		var a = moment(snap.val().firstTrain).format("X")
+		//current time
+		var b = moment().format("X");
+		//frequency
+		var c = moment(snap.val().freq).format("X")
 
-database.ref().on("value", function(snapshot) {
-	
-	for (i=0; i < snapshot.val().allTrains.length; i++) {
+		var d
 
-		$("#tableBody").html("<tr>" + "<td>" + snapshot.val().allTrains[i].name + "</td>" + "<tr>")
-		
-	}
+		for (i=0; i === -1; i++){
+			//adding the interval to the first train
+			a + c;
 
+			if (a > b) {
+				d = moment(a).format("HH:mm")
+				return
+			};
+		};
+		console.log(d)
+		return d
+	};
+
+	function calcAway(){
+		var a = moment(calcNextTrain()).format("X")
+		var b = moment().format("X")
+		var c = a.diff(b)
+		return c;
+	};
+
+	var row = $("<tr>")
+
+	var name = $("<td>" + snap.val().name + "</td>")
+	var dest = $("<td>" + snap.val().dest + "</td>")
+	var freq = $("<td>" + snap.val().freq + "</td>")
+	var nextTrain = $("<td>" + calcNextTrain() + "</td>")
+	var minAway = $("<td>" + calcAway() + "</td>")
+
+	row.append(name).append(dest).append(freq).append(nextTrain)
+
+	$("#tableBody").append(row)
 });
 
 
